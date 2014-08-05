130c279ee8e33d185f390aaa67775c3eed5e4a60
Endrit Bajo <endrit.bajo@gmail.com>
2014-08-05 20:42:37 +0200

    fix

--- movie_recommendations/dataset.py
+++ movie_recommendations/dataset.py
@@ -5,7 +5,7 @@ import urllib2
 data_url = "http://files.grouplens.org/datasets/movielens/ml-100k/u.data"
 movies_url = "http://files.grouplens.org/datasets/movielens/ml-100k/u.item"
 
-def getmovies():
+def get_movies_dataset():
   movies = {}
   # get movies
   movies_file = urllib2.urlopen(movies_url)
@@ -14,17 +14,13 @@ def getmovies():
     movies[id] = title
   return movies
 
-def getratings(bymovie = False):
+def get_ratings_dataset():
   data = {}
   # get user ratings
   data_file = urllib2.urlopen(data_url)
   for line in data_file:
     (user, movieid, rating, ts) = line.split('\t')
-    if bymovie:
-      data.setdefault(movieid, {})
-      data[movieid][user] = int(rating)
-    else:
-      data.setdefault(user, {})
-      data[user][movieid] = int(rating)
+    data.setdefault(user, {})
+    data[user][movieid] = int(rating)
 
   return data
--- movie_recommendations/recommendations.py
+++ movie_recommendations/recommendations.py
@@ -1,6 +1,91 @@
 __author__ = 'endrit bajo'
 
 import distances
+import utils
+
+class Dataset:
+  data = {}
+  similar_items = {}
+
+  def __init__(self, data):
+    self.data = data
+
+  def get():
+    return data
+
+  """
+  Transform dataset.
+  Transform the data from:
+
+  data = {
+    item1 : {subitem1: value_11, subitem2: value_12, ... subitemn: value_1n}
+    ...
+    itemn : {subitem1: value_n1, subitem2: value_n2, ... subitemn: value_nn}
+  }
+
+  to:
+
+  data = {
+    subitem1 : {item1: value_11, item2: value_12, ... itemn: value_1n}
+    ...
+    subitemn : {item1: value_n1, item2: value_n2, ... itemn: value_nn}
+  }
+
+  """
+  def flip():
+    result = {}
+
+    for item in data:
+      for subitem in data[item]:
+        result.setdefault(subitem, {})
+        # flip item and subitem
+        result[subitem][item] = data[item][subitem]
+
+    return result
+
+  """
+    Computes the dataset of similar items (item-based filtering).
+    Needs to be run once in a while to keep the similarity updated.
+    The fewer the data at disposal more frequent you need to run this method.
+  """
+  def build_similar_items_dataset(data, n = 5):
+    transformed_data = transform_dataset(data)
+
+    items = {}
+
+    for item in transformed_data:
+        items[item] = similar_items(transformed_data, item, similarity = distances.pearson_correlation, n = n)
+
+    return items
+
+"""
+  Given an item, it returns items similar to each other (similar movies or similar users).
+  If you want similar users, the data is a dictionary structured in the following way:
+
+  data = {
+    user1 : {movie1: rating1, movie2: rating2, ... movien: ratingn}
+    ...
+    usern : {movie1:rating1, movie2: rating2, ... movien: ratingn}
+  }
+
+  If you want similar movies:
+
+  data = {
+    movie1 : {user1: rating1, user2: rating2, ... usern: ratingn}
+    ...
+    movien : {user1: rating1, user2: rating2, ... usern: ratingn}
+  }
+
+"""
+def similar_items(data, myitem,
+                  similarity = distances.pearson_correlation,
+                  n = 5):
+  items = [(similarity(data, myitem, other), other) for other in data if other != myitem]
+
+  items.sort()
+  items.reverse()
+
+  return items[0:n]
 
 """
   Given an item, it returns items similar to each other (similar movies or similar users).
@@ -36,13 +121,13 @@ def similar_items(data, myitem,
 
 
 """
-  Given a user, it returns a list of recommended movies.
+  Given a user, it returns a list of recommended movies (user-based filtering).
   The data is a dictionary structured in the following way:
 
   data = {
     user1 : {movie1: rating1, movie2: rating2, ... movien: ratingn}
     ...
-    usern : {movie1:rating1, movie2: rating2, ... movien: ratingn}
+    usern : {movie1: rating1, movie2: rating2, ... movien: ratingn}
   }
 
 """
@@ -74,8 +159,7 @@ def recommended_movies(data,
           total_similarities.setdefault(movie, 0)
           total_similarities[movie] += sim
 
-  movies = [(total / total_similarities[movie], movie)
-                for (movie, total) in total_score.items()]
+  movies = [(total / total_similarities[movie], movie) for (movie, total) in total_score.items()]
 
   movies.sort()
   movies.reverse()
