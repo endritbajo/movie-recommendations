bd526128463bf0a0460f8c283112a65e60c58875
Endrit Bajo <endrit.bajo@gmail.com>
2015-02-10 19:39:23 +0100

    fix

--- movie_recommendations/dataset.py
+++ movie_recommendations/dataset.py
@@ -6,21 +6,21 @@ data_url = "http://files.grouplens.org/datasets/movielens/ml-100k/u.data"
 movies_url = "http://files.grouplens.org/datasets/movielens/ml-100k/u.item"
 
 def get_movies_dataset():
-  movies = {}
-  # get movies
-  movies_file = urllib2.urlopen(movies_url)
-  for line in movies_file:
-    (id, title) = line.split('|')[0:2]
-    movies[id] = title
-  return movies
+    movies = {}
+    # get movies
+    movies_file = urllib2.urlopen(movies_url)
+    for line in movies_file:
+        (id, title) = line.split('|')[0:2]
+        movies[id] = title
+    return movies
 
 def get_ratings_dataset():
-  data = {}
-  # get user ratings
-  data_file = urllib2.urlopen(data_url)
-  for line in data_file:
-    (user, movieid, rating, ts) = line.split('\t')
-    data.setdefault(user, {})
-    data[user][movieid] = int(rating)
+    data = {}
+    # get user ratings
+    data_file = urllib2.urlopen(data_url)
+    for line in data_file:
+        (user, movieid, rating, ts) = line.split('\t')
+        data.setdefault(user, {})
+        data[user][movieid] = int(rating)
 
-  return data
+    return data
